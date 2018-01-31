b989363d04023dacb9ad309a728c1c8b4a478ece
Lucas Burdell <lucas.burdell@blackburn.edu>
2018-01-31 17:07:14 -0600

    rewrite heuristics to be more accurate to their objective

--- aiheuristics/HighestMerges.java
+++ aiheuristics/HighestMerges.java
@@ -30,20 +30,27 @@ public class HighestMerges implements Heuristic {
 
     @Override
     public long getValueOfState(GameController controller, GameBoard state) {
-
-        GameNode[][] grid = state.getGameGrid();
+        Direction[] directions = Direction.values();
+        long output = 0;
+        for (int i = 0; i < directions.length; i++) {
+            GameBoard result = controller.moveGrid(state, directions[i]);
+            output += result.getScore() - state.getScore();
+        }
+        return output;
+        /*
+        int[][] grid = state.getGameGrid();
         long output = 0;
         for (int x = 0; x < GRID_SIZE - 1; x++) {
             for (int y = 0; y < GRID_SIZE - 1; y++) {
-                if (grid[x][y].getValue() == grid[x + 1][y].getValue()) {
-                    output += grid[x][y].getValue() * 2;
-                } else if (grid[x][y].getValue() == grid[x][y + 1].getValue()) {
-                    output += grid[x][y].getValue() * 2;
+                if (grid[x][y] == grid[x + 1][y]) {
+                    output += grid[x][y];
+                } else if (grid[x][y] == grid[x][y + 1]) {
+                    output += grid[x][y];
                 }
             }
         }
 
-        return output;
+        return output;*/
     }
 
 }
